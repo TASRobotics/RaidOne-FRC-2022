0dccf3797d4ff7a7decf4e8dbbe3b5a320b99c8e
24thomast <24thomast@students.tas.tw>
2021-03-26 16:27:27 +0800

    March 26th: Update On Slalom Path Sequence

--- src/main/java/raidone/robot/auto/sequences/SlalomPathSequence.java
+++ src/main/java/raidone/robot/auto/sequences/SlalomPathSequence.java
@@ -15,43 +15,49 @@ public class SlalomPathSequence extends AutoSequence {
 
     private static final Point[] FIRST_CURVE_WAYPOINTS = {
         new Point(0, 0, 0),
-        new Point(78, 53),
-        new Point(192, 63),
-        new Point(235, 30),
-        new Point(268, -2, 0)
+        new Point(45, 0),
+        new Point(50, 75),
+        new Point(90, 75),
+        new Point(150, 75, 0)
     };
     private static final Path FIRST_CURVE_PATH = new Path(FIRST_CURVE_WAYPOINTS, false,
         DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
 
     private static final Point[] SECOND_CURVE_WAYPOINTS = {
         new Point(0, 0, 0),
-        new Point(28, 49),
-        new Point(-2, 78),
-        new Point(-40, 39),
-        new Point(-133, -11, 180),
-        new Point(-198, -3),
-        new Point(-234, 35, 135),
-        new Point(-250, 60, 180)
+        new Point(42, 0),
+        new Point(83, -50),
+        new Point(118, -80, 0)
     };
     private static final Path SECOND_CURVE_PATH = new Path(SECOND_CURVE_WAYPOINTS, false,
         DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
     
-    // private static final Point[] THIRD_CURVE_WAYPOINTS = {
-    //     new Point(310, 30, -90),
-    //     new Point(270, 60, 180),
-    //     new Point(240, 30, 135),
-    //     new Point(150, 0, 180)
-    // };
+    private static final Point[] THIRD_CURVE_WAYPOINTS = {
+        new Point(0, 0, 0),
+        new Point(28, 49),
+        new Point(28, 73, 90)
+    };
+    private static final Path THIRD_CURVE_PATH = new Path(THIRD_CURVE_WAYPOINTS, false,
+        DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
+    
+    // // private static final Point[] THIRD_CURVE_WAYPOINTS = {
+    //     new Point(-40, 39),
+    //     new Point(-133, -11, 180),
+    //     new Point(-198, -3),
+    //     new Point(-234, 35, 135),
+    //     new Point(-250, 60, 180)
+    // // };
     // private static final Path THIRD_CURVE_PATH = new Path(THIRD_CURVE_WAYPOINTS, false,
     //     DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
     
-    // private static final Point[] FOURTH_CURVE_WAYPOINTS = {
-    //     new Point(150, 0, 180),
-    //     new Point(80, 10, -135),
-    //     new Point(0, 60, 180)
-    // };
-    // private static final Path FOURTH_CURVE_PATH = new Path(FOURTH_CURVE_WAYPOINTS, false,
-    //     DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
+    private static final Point[] FOURTH_CURVE_WAYPOINTS = {
+        new Point(0, 0, 90),
+        new Point(0, 10),
+        new Point(-62, -30, 270),
+        new Point(-62, -40, 270)
+    };
+    private static final Path FOURTH_CURVE_PATH = new Path(FOURTH_CURVE_WAYPOINTS, false,
+        DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
     
     public SlalomPathSequence() {
         System.out.println(DriverStation.getInstance().getAlliance().name());
@@ -70,17 +76,17 @@ public class SlalomPathSequence extends AutoSequence {
                     Arrays.asList(
                         new DrivePath(SECOND_CURVE_PATH)
                     )
+                ),
+                new ParallelAction(
+                    Arrays.asList(
+                        new DrivePath(THIRD_CURVE_PATH)
+                    )
+                ),
+                new ParallelAction(
+                    Arrays.asList(
+                        new DrivePath(FOURTH_CURVE_PATH)
+                    )
                 )
-                // new ParallelAction(
-                //     Arrays.asList(
-                //         new DrivePath(THIRD_CURVE_PATH)
-                //     )
-                // ),
-                // new ParallelAction(
-                //     Arrays.asList(
-                //         new DrivePath(FOURTH_CURVE_PATH)
-                //     )
-                // )
             )
         ));
         System.out.println("Added actions.");

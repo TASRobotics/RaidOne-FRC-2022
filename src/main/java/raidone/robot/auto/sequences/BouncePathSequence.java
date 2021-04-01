5000d7aa1344ec53c5f0cd091b184d9191df7938
Ashmita Haldar <67264881+ashmitahaldar@users.noreply.github.com>
2021-04-01 12:04:39 +0800

    April 1st: Updated Bounce Path Points

--- src/main/java/raidone/robot/auto/sequences/BouncePathSequence.java
+++ src/main/java/raidone/robot/auto/sequences/BouncePathSequence.java
@@ -21,27 +21,28 @@ public class BouncePathSequence extends AutoSequence {
 
     private static final Point[] FIRST_CURVE_WAYPOINTS = {
         new Point(0, 0, 0),
-        new Point(50, 100, 90) //A3
+        new Point(45, 60, 90) //A3 -- (50, 100, 90)
     };
     private static final Path FIRST_CURVE_PATH = new Path(FIRST_CURVE_WAYPOINTS, false,
         DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
 
     private static final Point[] SECOND_CURVE_WAYPOINTS = {
-        new Point(0, 0, 90), //lowered x coordinates by 15
-        new Point(-15, 100),
-        new Point(-60, 200, 180),
-        new Point(-110, 100),
-        new Point(-135, 0, -90) //A6
+        new Point(0, 0, 90), //comments are old points for reference purposes
+        new Point(-30, 95), //(-15, 100)
+        new Point(-60, 120, 180), //(-60, 200, 180)
+        new Point(-85, 95),//(-110, 100)
+        new Point(-90, 0, -90) //A6 -- (-135, 0, -90)
     };
     private static final Path SECOND_CURVE_PATH = new Path(SECOND_CURVE_WAYPOINTS, true,
     DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
 
     private static final Point[] THIRD_CURVE_WAYPOINTS = {
         new Point(0, 0, -90),
-        new Point(15, -100),
-        new Point(60, -200, 0),
-        new Point(110, -100),
-        new Point(135, 0, 90) //A9
+        new Point(10, -95), //(15, -100)
+        new Point(30, -110, 0), //(60, -200, 0)
+        new Point(60, -110), //(110, -100)
+        new Point(80, -95), //newly added
+        new Point(90, 0, 90) //A9 -- (135, 0, 90)
     };
     private static final Path THIRD_CURVE_PATH = new Path(THIRD_CURVE_WAYPOINTS, false,
     DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
@@ -49,7 +50,7 @@ public class BouncePathSequence extends AutoSequence {
     private static final Point[] FOURTH_CURVE_WAYPOINTS = {
         new Point(0, 0, 90),
         //new Point(-35, 65),
-        new Point(-50, 65, 180)
+        new Point(-60, 60, 180) //(-50, 65, 180)
     };
     private static final Path FOURTH_CURVE_PATH = new Path(FOURTH_CURVE_WAYPOINTS, true,
     DriveConstants.DEFAULT_CRUISE_VELOCITY, DriveConstants.DEFAULT_TARGET_ACCELERATION);
